8d645119a99e8cffb5f658d841a0885b85877f19
Daniel Krajnak <dan.krajnak@gmail.com>
2020-10-25 21:36:19 -0400

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+const Home = () => (
+  <>
+    <div className="holder">
+      <div>
+        <h1>Love Lines</h1>
+        <div className="description">Under construction</div>
+      </div>
+    </div>
+    <style jsx>{`
+      .holder,
+      .holder > div {
+        display: flex;
+        width: 100%;
+        height: 100vh;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+      }
+      h1 {
+        color: #d95525;
+        margin-bottom: 3px;
+      }
+      .description {
+        font-weight: lighter;
+      }
+    `}</style>
+  </>
+);
+
+export default Home;
