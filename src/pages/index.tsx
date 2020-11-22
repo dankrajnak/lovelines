e70fc645730c682888293729ed2b58366691787d
Daniel Krajnak <dan.krajnak@gmail.com>
2020-11-21 21:49:03 -0500

    Try out new vibe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,30 @@
 import SEO from "../utilities/SEO";
 
+const Circle = (props: { color: string }) => {
+  const radius = 5;
+  return (
+    <svg width={radius * 2} height={radius * 2} style={{ marginLeft: 5 }}>
+      <circle
+        r={radius}
+        cx={radius}
+        cy={radius}
+        style={{ fill: props.color }}
+      />
+    </svg>
+  );
+};
+
 const Home = () => (
   <>
     <SEO />
     <div className="holder">
       <div>
-        <h1>Love Lines</h1>
+        <h1>
+          LoveLines
+          <Circle color="#D95525" />
+          <Circle color="#6D99AD" />
+          <Circle color="#D7A164" />
+        </h1>
         <div className="description">Under construction</div>
       </div>
     </div>
@@ -20,8 +39,8 @@ const Home = () => (
         align-items: center;
       }
       h1 {
-        color: #d95525;
         margin-bottom: 3px;
+        font-weight: 500;
       }
       .description {
         font-weight: lighter;
