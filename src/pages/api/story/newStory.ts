94f8a5847bf25f2d69e1e11f29f5c8a3f37c9096
Daniel Krajnak <dan.krajnak@gmail.com>
2020-12-20 01:41:03 -0500

    WIP save story

--- src/pages/api/story/newStory.ts
+++ src/pages/api/story/newStory.ts
@@ -1,18 +1,70 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
 import prisma from "../../../Server/prisma";
+import { validateRequestBodyUnsafe } from "../../../Server/validateRequest";
+import { APINewStoryRequestSchema } from "../../../Server/apiSchemas";
+
+export type NewStoryAPILine = {
+  /**
+   *
+   * @minimum 1900
+   */
+  year: number;
+  /**
+   * @maximum 12
+   * @minimum 1
+   */
+  month: number;
+  isHeartBreak: boolean;
+  intensity: number;
+};
+
+export type APINewStoryRequest = {
+  story: NewStoryAPILine[][];
+};
 
 const newStory = async (
   req: NextApiRequest,
   res: NextApiResponse<any>
 ): Promise<void> => {
-  const session = await getSession({ req });
+  const data = validateRequestBodyUnsafe<APINewStoryRequest>(
+    req,
+    res,
+    APINewStoryRequestSchema
+  );
+  if (data) {
+    const session = await getSession({ req });
+    const now = new Date();
+    if (session?.user.email) {
+      const story = await prisma.story.create({
+        data: {
+          createdDate: now,
+          modifiedDate: now,
+          person: {
+            connect: { email: session.user.email },
+          },
+          line: {
+            create: data.story.map((line) => ({
+              createdDate: now,
+              periods: {
+                create: line.map(
+                  ({ year, month, isHeartBreak, intensity }) => ({
+                    year,
+                    yearMonth: year * 100 + month,
+                    month,
+                    isHeartBreak,
+                    intensity,
+                  })
+                ),
+              },
+            })),
+          },
+        },
+      });
 
-  const person = await prisma.person.findUnique({
-    where: { email: session?.user?.email || undefined },
-  });
-
-  res.json(person);
+      res.json(story);
+    }
+  }
 };
 
 export default newStory;
